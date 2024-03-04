c7e4c069ec85960ae1229b881b36f42e79b2613e
unknown <eskicmihajlo55@gmail.com>
2024-03-04 23:41:29 +0100

    added reaction with checkpoint and finishing the game

--- script.js
+++ script.js
@@ -311,6 +311,56 @@ const animate = () => {  /// player movement  animation  by pressing keys
             player.velocity.y = gravity;
             
         }
+
+
+
+        checkpoints.forEach((checkpoint,index,checkpoints  )=>{     //// detection checkpoint
+
+            const checkpointDetectionRules = [
+
+                player.position.x >= checkpoint.position.x,
+                player.position.y >= checkpoint.position.y ,
+                player.position.y + player.height <= checkpoint.position.y + checkpoint.height,
+                isCheckpointCollisionDetectionActive,
+
+                player.position.x - player.width <= checkpoint.position.x - checkpoint.width + player.width * 0.9,
+                index === 0 || checkpoints[index - 1].claimed === true,
+
+
+            ];
+
+            if(checkpointDetectionRules.every((rule)=> rule))    /// claim checkpoint
+            {
+
+                
+                
+                checkpoint.claim();
+
+                if(index === checkpoints.length-1)
+                {
+                    isCheckpointCollisionDetectionActive = false;
+                    
+                    showCheckpointScreen("You reached the final checkpoint!");
+                    movePlayer("ArrowRight", 0,false);
+                }
+                else if (player.position.x >= checkpoint.position.x && player.position.x <= checkpoint.position.x +40)
+                {
+                    showCheckpointScreen("You reached a checkpoint!");
+                }
+
+            }
+
+
+            
+        
+
+            
+
+
+            
+
+
+        })
         
 
 
@@ -382,6 +432,25 @@ const startGame = () => {
     animate();
 
 }
+
+
+const showCheckpointScreen = (msg) => {
+
+    checkpointScreen.style.display = "block";
+    checkpointMessage.textContent = msg;
+
+
+    if(isCheckpointCollisionDetectionActive)
+    {
+        setTimeout(() => {
+            checkpointScreen.style.display = "none";
+        }, 2000);
+    }
+
+
+
+}
+
 
 startBtn.addEventListener("click", startGame)
 
